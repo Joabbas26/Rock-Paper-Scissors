6f8522937b2151bb355cf0105bd10611edf75a00
Joab <joabbastidas@gmail.com>
2020-06-25 17:45:30 -0400

    Re-structured design and fixed a few bugs

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,74 +1,82 @@
+function zoomImage(choice){
+    choice.style.height = "250px";
+    choice.style.width = "250px";
+    choice.style.cursor = "pointer";
+}
+
+function zoomOutImage(choice){
+    choice.style.height = "200px";
+    choice.style.width = "200px";
+}
+
 //Function should detect which image was clicked, keep it at the 
 //same size and move on to the next function
-function userChoice(user){
+var User = function userChoice(user){
     var user;
-    if (this === document.getElementByClassName('userRock')) {
+    if (this === document.getElementByClassName('.userRock')) {
         this.style.width = "250px";
         this.style.height = "250px";
-        user = 0;
-        return user;
+        return 0;
     }
-    else if (this === document.getElementByClassName('userPaper')) {
-        var paper = document.getElementByClassName('userPaper');
-        paper.style.width = "250px";
-        paper.style.height = "250px";
-        user = 1;
-        return user;
+    else if (this === document.getElementByClassName('.userPaper')) {
+        this.style.width = "250px";
+        this.style.height = "250px";
+        return 1;
     }
     else {
         this.style.width = "250px";
         this.style.height = "250px";
-        user = 2;
-        return user;
+        return 2;
     }
-    function computerChoice();
-    function compare();
+    computerChoice();
+    compare(computer, user);
 }
+
 
 //Generates a random number and uses it to assign 0, 1 or 2 to rock, 
 //paper and scissors respectabley
 function computerChoice(computer){
-    var computerChoice = Math.random();
-    if (computerChoice < 0.34) {
-        computerChoice = 0;
-        return computerChoice;
+    var computer = Math.random();
+    if (computer < 0.34) {
+        computer = 0;
+        return computer;
     } 
-    else if(computerChoice <= 0.67) {
-        computerChoice = 1;
-        return computerChoice;
+    else if(computer <= 0.67) {
+        computer = 1;
+        return computer;
     } 
     else {
-        computerChoice = 2;
-        return computerChoice;
+        computer = 2;
+        return computer;
     }
 }
 
-//Function is supposed to take the returned value of both userChoice 
-//and computerChoice and compare them to see who wins
+//Function is supposed to take the returned value of both user 
+//and computer and compare them to see who wins
 function compare(computer, user){
-    if(userChoice === computerChoice) {
+    if(user === computer) {
         alert("The result is a tie!");
-}
-if(userChoice === 0) {
-    if(computerChoice === 2) {
-        alert("rock wins");
-    } else {
-        alert("paper wins");
     }
-}
-if(userChoice === 1) {
-    if(computerChoice === 0) {
+    if(user === 0) {
+        if(computer === 2) {
+        alert("rock wins");
+     } else {
+        alert("paper wins");
+        }
+    }
+if(user === 1) {
+    if(computer === 0) {
         alert("paper wins");
     } else {
-        if(computerChoice === 2) {
+        if(computer === 2) {
             alert("scissors wins");
     }
 }
-if(userChoice === 2) {
-    if(computerChoice === 0) {
+if(user === 2) {
+    if(computer === 0) {
         alert("rock wins");
     } else {
-        if(computerChoice === 1) {
+        if(computer === 1) {
             alert("scissors wins");
         }
     }
