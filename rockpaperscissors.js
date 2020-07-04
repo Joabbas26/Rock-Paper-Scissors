668e4763634b5a8b3d7fd36cb2d014fb3da2a467
Joab <joabbastidas@gmail.com>
2020-07-04 14:53:44 -0400

    Removed onmouseover and onmouseout, added scores and rearranged event listeners

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,4 +1,4 @@
-function zoomImage(choice){
+/*function zoomImage(choice){
     choice.style.height = "250px";
     choice.style.width = "250px";
     choice.style.cursor = "pointer";
@@ -9,6 +9,10 @@ function zoomOutImage(choice){
     choice.style.width = "200px";
 }
 
+onmouseover="zoomImage(this)" onmouseout="zoomOutImage(this)"
+
+*/
+
 //Used to calculate the score of each player
 var playerScore = 0;
 var cpuScore = 0;
@@ -16,31 +20,42 @@ var cpuScore = 0;
 //Should detect which image was clicked, keep it at the 
 //same size and set user variable to respective number
 var user = 0;
-var rockChoice = document.getElementByClassName('.userRock');
-var paperChoice = document.getElementByClassName('.userPaper');
-var scissorsChoice = document.getElementByClassName('.userScissors');
 
-var onRockChoice = function(user){
+document.getElementById('playerScore').innerHTML = `Player: ${playerScore}`;
+document.getElementById('cpuScore').innerHTML = `CPU: ${cpuScore}`;
+
+var rockChoice = document.getElementsByClassName('.userRock');
+var paperChoice = document.getElementsByClassName('.userPaper');
+var scissorsChoice = document.getElementsByClassName('.userScissors');
+
+function onRockChoice(user){
     rockChoice.style.width = "250px";
     rockChoice.style.height = "250px";
+    rockChoice.style.cursor = "pointer";
     return 0;
+    computerChoice(computer);
+    compare(computer, user);
 }
 
-var onPaperChoice = function(user){
+function onPaperChoice(user){
     paperChoice.style.width = "250px";
     paperChoice.style.height = "250px";
     return 1;
+    computerChoice();
+    compare();
 }
 
-var onScissorsChoice = function(user){
+ function onScissorsChoice(user){
     scissorsChoice.style.width = "250px";
     scissorsChoice.style.height = "250px";
     return 2;
+    computerChoice();
+    compare();
 }
 
-rockChoice.addEventListener("click", onRockChoice);
-paperChoice.addEventListener("click", onPaperChoice);
-scissorsChoice.addEventListener("click", onScissorsChoice);
+rockChoice.addEventListener("click", onRockChoice());
+paperChoice.addEventListener("click", onPaperChoice());
+scissorsChoice.addEventListener("click", onScissorsChoice());
 
 
 //Generates a random number and uses it to assign 0, 1 or 2 to rock, 
@@ -70,24 +85,30 @@ function compare(computer, user){
     if(user === 0) {
         if(computer === 2) {
         alert("rock wins");
+        playerScore += 1;
      } else {
         alert("paper wins");
+        cpuScore += 1;
         }
     }
 if(user === 1) {
     if(computer === 0) {
         alert("paper wins");
+        playerScore += 1;
     } else {
         if(computer === 2) {
             alert("scissors wins");
+            cpuScore += 1;
     }
 }
 if(user === 2) {
     if(computer === 0) {
         alert("rock wins");
+        cpuScore += 1;
     } else {
         if(computer === 1) {
             alert("scissors wins");
+            playerScore += 1;
         }
     }
 }
